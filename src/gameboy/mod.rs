eb9cd82e99ccde3943da3346df879edfefa6c021
NoOverflow <julien.prost@epitech.eu>
2022-02-19 22:56:41 +0100

    Add Gameboy BIOS

---
+++ src/gameboy/mod.rs
@@ -0,0 +1,17 @@
+pub mod cpu;
+
+use cpu::Cpu;
+
+pub struct Gameboy {
+    cpu: Cpu,
+}
+
+impl Gameboy {
+    pub fn new() -> Self {
+        Gameboy { cpu: Cpu::new() }
+    }
+
+    pub fn dump_bios(&self) {
+        self.cpu.dump_bios();
+    }
+}
