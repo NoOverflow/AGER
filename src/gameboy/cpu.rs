8c314e51a429bca7dffc4dff1c63a596ce24818f
NoOverflow <julien.prost@epitech.eu>
2022-02-20 19:47:12 +0100

    Add fetch(s) methods

--- src/gameboy/cpu.rs
+++ src/gameboy/cpu.rs
@@ -12,6 +12,19 @@ impl Cpu {
         }
     }
 
+    pub fn fetch_u8(&mut self, mem: &mut Memory) -> u8 {
+        let v: u8 = mem.read_u8(self.registers.pc as usize);
+
+        self.registers.pc += 1;
+        v
+    }
+
+    pub fn fetch_u16(&mut self, mem: &mut Memory) -> u16 {
+        let low: u16 = self.fetch_u8(mem) as u16;
+        let high: u16 = self.fetch_u8(mem) as u16;
+
+        return high << 8 | low;
+    }
     pub fn cycle(&mut self, mem: &mut Memory) {
         let inst_op_code: u8 = mem.read_u8(self.registers.pc as usize);
 
