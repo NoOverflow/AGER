8bf362e5f594aa2f27077d0fce5dffd33052beda
NoOverflow <julien.prost@epitech.eu>
2022-02-21 21:04:28 +0100

    Fix broken 16bits <-> 8bits bitwise operations

--- src/gameboy/bin_utils.rs
+++ src/gameboy/bin_utils.rs
@@ -2,25 +2,25 @@ pub struct BinUtils {}
 
 impl BinUtils {
     pub fn u16_from_u8s(high: u8, low: u8) -> u16 {
-        return (low as u16) | ((high as u16) << 4);
+        return (low as u16) | ((high as u16) << 8);
     }
 
     pub fn u8s_from_u16(v: u16) -> (u8, u8) {
-        ((v >> 4) as u8, ((v & 0xF) as u8))
+        ((v >> 8) as u8, ((v & 0xFF) as u8))
     }
 }
 
 #[test]
 fn get_u8s_from_u16() {
-    let result: (u8, u8) = BinUtils::u8s_from_u16(0xAD);
+    let result: (u8, u8) = BinUtils::u8s_from_u16(0xADEF);
 
-    assert_eq!(result.0, 0xA);
-    assert_eq!(result.1, 0xD);
+    assert_eq!(result.0, 0xAD);
+    assert_eq!(result.1, 0xEF);
 }
 
 #[test]
 fn get_u16_from_u8s() {
-    let result: u16 = BinUtils::u16_from_u8s(0xA, 0xD);
+    let result: u16 = BinUtils::u16_from_u8s(0xAD, 0xEF);
 
-    assert_eq!(result, 0xAD);
+    assert_eq!(result, 0xADEF);
 }
