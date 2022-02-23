c6bb83aad94894285b990623e53aa246f6901918
NoOverflow <julien.prost@epitech.eu>
2022-02-23 01:06:20 +0100

    Remove mount_boot flawed logic

--- src/gameboy/mod.rs
+++ src/gameboy/mod.rs
@@ -21,18 +21,11 @@ impl Gameboy {
 
     pub fn power_up(&mut self) {
         self.cpu.registers.pc = 0x0;
-        self.mount_boot();
         self.cpu.registers.sp = 0xFFFE;
         self.mem_map.is_booting = true;
     }
 
     pub fn cycle(&mut self) {
         self.cpu.cycle(&mut self.mem_map);
-    }
-
-    fn mount_boot(&mut self) {
-        for i in 0..256 {
-            self.mem_map.write_u8(self.boot[i], i);
-        }
     }
 }
