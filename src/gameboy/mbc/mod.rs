9579926c23a1d69a331758acff5db69512234965
NoOverflow <julien.prost@epitech.eu>
2022-02-24 19:53:40 +0100

    Add MemoryBankController trait and define Mbcs IDs

---
+++ src/gameboy/mbc/mod.rs
@@ -0,0 +1,13 @@
+pub mod mbc0;
+
+pub enum MbcId {
+    MBC0 = 0x0,
+    MBC1 = 0x1,
+    MBC1WRAM = 0x2,
+    // TODO: Finish
+}
+
+pub trait MemoryBankController {
+    fn read_u8(&self, address: usize) -> u8;
+    fn write_u8(&mut self, address: usize, v: u8);
+}
