ad8513a9689bb037acf7d88e4f94c251c3baf297
NoOverflow <julien.prost@epitech.eu>
2022-02-28 22:42:58 +0100

    Add a window using minifb

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,31 @@
+extern crate minifb;
 mod gameboy;
 
 use gameboy::Gameboy;
+use minifb::{Key, Window, WindowOptions};
+
+const WINDOW_WIDTH: usize = 160 * 4;
+const WINDOW_HEIGHT: usize = 144 * 4;
 
 fn main() {
     let gb: &mut Gameboy = &mut Gameboy::new();
+    let mut buffer: Vec<u32> = vec![0; WINDOW_WIDTH * WINDOW_HEIGHT];
+    let mut window = Window::new(
+        "AGER",
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        WindowOptions::default(),
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
 
     gb.load_cartridge("res/test/licensed/tetris.gb");
     gb.power_up();
-    loop {
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        window
+            .update_with_buffer(&buffer, WINDOW_WIDTH, WINDOW_HEIGHT)
+            .unwrap();
         gb.cycle();
     }
 }
