d8b96ff7243d1ca6828aa986d10ef66aa2aa2da0
NoOverflow <julien.prost@epitech.eu>
2022-03-27 22:29:05 +0200

    Add HRAM and WRAM support

--- src/gameboy/memory.rs
+++ src/gameboy/memory.rs
@@ -29,18 +29,19 @@ pub struct Memory {
     rom_address_bound: Range<usize>,
     vram_address_bound: Range<usize>,
     sram8_address_bound: Range<usize>,
-    iram8_address_bound: Range<usize>,
+    wram_address_bound: Range<usize>,
     iram8_echo_address_bound: Range<usize>,
-    sam_address_bound: Range<usize>,
+    oam_address_bound: Range<usize>,
     io_address_bound: Range<usize>,
-    iram_address_bound: Range<usize>,
+    hram_address_bound: Range<usize>,
 
     // Memory
     boot: &'static [u8; 256],
     pub rom: Box<dyn MemoryBankController>,
     vram: [u8; 0x2000],
-    iram: [u8; 0x128],
-    iram8: [u8; 0x2000],
+    hram: [u8; 0x7F],
+    wram: [u8; 0x2000],
+    oam: [u8; 0x100],
 
     // TODO: Sort this, maybe export ?
     // Special Registers
@@ -73,7 +74,7 @@ impl Memory {
                 start: 0xA000,
                 end: 0xC000,
             },
-            iram8_address_bound: Range {
+            wram_address_bound: Range {
                 start: 0xC000,
                 end: 0xE000,
             },
@@ -81,15 +82,15 @@ impl Memory {
                 start: 0xE000,
                 end: 0xFE00,
             },
-            sam_address_bound: Range {
+            oam_address_bound: Range {
                 start: 0xFE00,
-                end: 0xFEA0,
+                end: 0xFF00,
             },
             io_address_bound: Range {
                 start: 0xFF00,
-                end: 0xFF4C,
+                end: 0xFF80,
             },
-            iram_address_bound: Range {
+            hram_address_bound: Range {
                 start: 0xFF80,
                 end: 0xFFFF,
             },
@@ -98,8 +99,9 @@ impl Memory {
             boot: include_bytes!("../../res/boot.bin"),
             rom: Box::new(MBC0::new([].to_vec())), // By default we "load" a MBC0
             vram: [0; 0x2000],
-            iram: [0; 0x128],
-            iram8: [0; 0x2000],
+            hram: [0; 0x7F],
+            wram: [0; 0x2000],
+            oam: [0; 0x100],
 
             // Special registers
             boot_rom_enable: 0,
@@ -141,12 +143,14 @@ impl Memory {
     pub fn write_u8(&mut self, value: u8, address: usize) {
         if self.rom_address_bound.contains(&address) {
             self.rom.write_u8(address, value);
-        } else if self.iram_address_bound.contains(&address) {
-            self.iram[address - self.iram_address_bound.start] = value;
+        } else if self.hram_address_bound.contains(&address) {
+            self.hram[address - self.hram_address_bound.start] = value;
         } else if self.vram_address_bound.contains(&address) {
             self.vram[address - self.vram_address_bound.start] = value;
-        } else if self.iram8_address_bound.contains(&address) {
-            self.iram8[address - self.iram8_address_bound.start] = value;
+        } else if self.wram_address_bound.contains(&address) {
+            self.wram[address - self.wram_address_bound.start] = value;
+        } else if self.oam_address_bound.contains(&address) {
+            self.oam[address - self.oam_address_bound.start] = value;
         } else if address == 0xFF50 || address == 0xFFFF || self.io_address_bound.contains(&address)
         {
             self.write_io_u8(value, address);
@@ -165,10 +169,14 @@ impl Memory {
                 return self.boot[address];
             }
             return self.rom.read_u8(address);
-        } else if self.iram_address_bound.contains(&address) {
-            return self.iram[address - self.iram_address_bound.start];
+        } else if self.hram_address_bound.contains(&address) {
+            return self.hram[address - self.hram_address_bound.start];
         } else if self.vram_address_bound.contains(&address) {
             return self.vram[address - self.vram_address_bound.start];
+        } else if self.wram_address_bound.contains(&address) {
+            return self.wram[address - self.wram_address_bound.start];
+        } else if self.oam_address_bound.contains(&address) {
+            return self.oam[address - self.oam_address_bound.start];
         } else if self.io_address_bound.contains(&address) {
             return self.read_io_u8(address);
         } else {
