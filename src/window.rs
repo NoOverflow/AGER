2caec5bd3576d22a74ea1f242648bfcfeec6f17b
NoOverflow <julien.prost@epitech.eu>
2022-09-09 21:00:36 +0200

    Move towards GTK renderer

--- src/window.rs
+++ src/window.rs
@@ -1,12 +1,51 @@
+use core::ffi::c_void;
+use glium::backend::Context;
 use glium::glutin::dpi::{LogicalSize, PhysicalSize};
 use glium::glutin::event::{Event, WindowEvent};
 use glium::glutin::platform::windows::WindowBuilderExtWindows;
 use glium::Surface;
+use glium::SwapBuffersError;
+use gtk::prelude::*;
 use std::sync::mpsc::Receiver;
+
+use gio::prelude::*;
+use std::env::args;
 
 const SCALE: u32 = 4;
 const WINDOW_WIDTH: u32 = 160; // 160 * SCALE;
 const WINDOW_HEIGHT: u32 = 144; // 144 * SCALE;
+
+struct GLAreaBackend {
+    glarea: gtk::GLArea,
+}
+
+unsafe impl glium::backend::Backend for GLAreaBackend {
+    fn swap_buffers(&self) -> Result<(), SwapBuffersError> {
+        // GTK swaps the buffers after each "render" signal itself
+        Ok(())
+    }
+    unsafe fn get_proc_address(&self, symbol: &str) -> *const c_void {
+        gl_loader::get_proc_address(symbol) as *const _
+    }
+    fn get_framebuffer_dimensions(&self) -> (u32, u32) {
+        // let allocation = self.glarea.get_allocation();
+        //(allocation.width as u32, allocation.height as u32)
+        (700, 700)
+    }
+    fn is_current(&self) -> bool {
+        // GTK makes it current itself on each "render" signal
+        true
+    }
+    unsafe fn make_current(&self) {
+        self.glarea.make_current();
+    }
+}
+
+impl GLAreaBackend {
+    fn new(glarea: gtk::GLArea) -> Self {
+        Self { glarea }
+    }
+}
 
 fn create_window_builder() -> glium::glutin::window::WindowBuilder {
     return glium::glutin::window::WindowBuilder::new()
@@ -66,6 +105,66 @@ fn draw_buffer(
 }
 
 pub fn init_window(rx: Receiver<Vec<u32>>) {
+    let application =
+        gtk::Application::new(Some("com.github.gtk-rs.examples.basic"), Default::default());
+
+    application.connect_activate(init_window_gtk);
+    application.run();
+}
+
+fn init_window_gtk(application: &gtk::Application) {
+    let window = gtk::ApplicationWindow::new(application);
+
+    window.set_title("AGER");
+    window.set_border_width(0);
+    window.set_position(gtk::WindowPosition::Center);
+    window.set_default_size(
+        WINDOW_WIDTH as i32 * SCALE as i32,
+        WINDOW_HEIGHT as i32 * SCALE as i32,
+    );
+
+    let glarea = gtk::GLArea::new();
+
+    window.add(&glarea);
+    window.show_all();
+
+    gl_loader::init_gl();
+    let context = unsafe {
+        Context::new(
+            GLAreaBackend::new(glarea.clone()),
+            true,
+            glium::debug::DebugCallbackBehavior::DebugMessageOnError,
+        )
+        .unwrap()
+    };
+
+    let counter = std::rc::Rc::new(std::cell::RefCell::new(0u32));
+
+    glarea.connect_render(move |_glarea, _glcontext| {
+        let mut frame = glium::Frame::new(context.clone(), context.get_framebuffer_dimensions());
+        // this is where you can do your glium rendering
+
+        let c = *counter.borrow() as f32 / 100.;
+        let r = c.sin() / 2. + 0.5;
+        let g = (c * 1.25).sin() / 2. + 0.5;
+        let b = (c * 1.5).sin() / 2. + 0.5;
+        frame.clear_color(r, g, b, 1.0);
+
+        frame.finish().unwrap();
+        *counter.borrow_mut() += 1;
+        Inhibit(true)
+    });
+
+    // This makes the GLArea redraw 60 times per second
+    // You can remove this if you want to redraw only when focused/resized
+    /*const FPS: u32 = 60;
+    glib::source::timeout_add_local(1_000 / FPS, move || {
+        glarea.queue_draw();
+        glib::source::Continue(true)
+    });*/
+}
+
+pub fn init_window_old(rx: Receiver<Vec<u32>>) {
     let window_builder = create_window_builder();
     let context_builder = glium::glutin::ContextBuilder::new();
     let event_loop = glium::glutin::event_loop::EventLoop::new();
